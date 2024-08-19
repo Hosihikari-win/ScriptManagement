646c6e8978cf9051cc65af90642e88aa01cc277e
予纾 <100036558+Yuisyuu@users.noreply.github.com>
2024-08-19 15:38:15 +0800

    fix: lua engine unloading

--- src/LuaPlugin.cs
+++ src/LuaPlugin.cs
@@ -5,29 +5,21 @@ namespace Hosihikari.ScriptManagement;
 
 public class LuaPlugin(FileInfo fileInfo) : Plugin(fileInfo)
 {
-    private LuaChunk? _luaChunk;
+    private readonly Lua _lua = new();
 
     protected override void Initialize()
     {
-        using Lua lua = new();
-        _luaChunk = lua.CompileChunk(_fileInfo.FullName, new());
+        // Nothing need to prepare
     }
 
     protected override void Load()
     {
-        if (_luaChunk is null)
-        {
-            throw new NullReferenceException();
-        }
-        _luaChunk.Run([]);
+        LuaChunk luaChunk = _lua.CompileChunk(_fileInfo.FullName, new());
+        luaChunk.Run([]);
     }
 
     protected override void Unload()
     {
-        if (_luaChunk is null)
-        {
-            throw new NullReferenceException();
-        }
-        _luaChunk.Lua.Clear();
+        _lua.Dispose();
     }
 }
